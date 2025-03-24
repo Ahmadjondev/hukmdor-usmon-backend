60071728246d90bbf74b0e8521aa31d845475beb
ahmadjondev <ahmadjondevv@gmail.com>
2025-03-24 09:16:21 +0600

    remove commented-out schema view and unused imports in urls and views

--- HukmdorUsmon/urls.py
+++ HukmdorUsmon/urls.py
@@ -14,31 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from HukmdorUsmon import settings
 
 from HukmdorUsmon import settings
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="HukmdorUsmon API",
-        default_version='v1',
-        description="API documentation for HukmdorUsmon",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="ahmadjondevv@gmail.com"),
-        license=openapi.License(name="2BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="HukmdorUsmon API",
+#         default_version='v1',
+#         description="API documentation for HukmdorUsmon",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="ahmadjondevv@gmail.com"),
+#         license=openapi.License(name="2BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 urlpatterns = [
                   path('manager/', admin.site.urls),
                   path('api/', include('series.urls')),
